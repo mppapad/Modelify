6eae6cd3ea05c3aad7d9c359e9ae5baf57d78527
Miltiadis Papadopoulos <miltospap5@gmail.com>
2025-05-06 18:15:05 +0300

    Favicon fix
     - Changed the default favicon

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,16 @@
 import { AppSidebar } from "@/components/app-sidebar";
 import { SiteHeader } from "@/components/site-header";
 import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Modelify",
+  description: "Effortlessly showcase and interact with your 3D models",
+  icons: {
+    icon: "/favicon.ico",
+  },
+};
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <>
